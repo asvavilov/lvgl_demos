3d5a464f3b7e5da43b4c16efedf41b81663bb3e7
asvavilov <vavilov.rf@gmail.com>
2022-03-12 15:41:42 +0300

    add readme

---
+++ README.md
@@ -0,0 +1,14 @@
+# Эксперименты с LVGL
+
+
+TODO попробовать кириллические шрифты
+
+
+инструкция по LVGL: https://docs.lvgl.io/master/get-started/arduino.html
+
+
+esp32 doit devkit 1.0:
+
+ili9341: подключение VSPI и задать прочие пины TFT_CS, TFT_DC, TFT_RST, см. пример Setup1_ILI9341.h
+
+ili9844: подключение VSPI и задать прочие пины по примеру Setup21_ILI9488.h
--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,5 @@
 #include <Arduino.h>
 
-
-// TODO попробовать кириллические шрифты
-
-
-/*
-инструкция по LVGL: https://docs.lvgl.io/latest/en/html/get-started/arduino.html#
-
-esp32 doit devkit 1.0:
-ili9341: подключение VSPI и задать прочие пины TFT_CS, TFT_DC, TFT_RST, см. пример Setup1_ILI9341.h
-ili9844: подключение VSPI и задать прочие пины по примеру Setup21_ILI9488.h
-*/
-
 #include <TFT_eSPI.h>
 
 #include <lvgl.h>
